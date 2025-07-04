29993869bd0e6be17839b8f1526d4d5d97e78da8
Ali-Hady <aliabdelhady43@gmail.com>
2025-07-05 00:38:53 +0300

    Minor improvements to ShippedProduct

--- src/ShippedProduct.java
+++ src/ShippedProduct.java
@@ -1,35 +1,5 @@
-public class ShippedProduct {
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    private String name;
-    private int quantity;
-    private double price;
-
+public class ShippedProduct extends Product {
     public ShippedProduct(String name, double price, int quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
+        super(name, price, quantity);
     }
 }
